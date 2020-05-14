485b31c255a0ceb9f8b947c8bab0d17b4e56991c
hjl <2869773733@qq.com>
2020-05-14 13:16:19 +0800

    card

--- app/src/main/java/com/example/lastone/MyListActivity.java
+++ app/src/main/java/com/example/lastone/MyListActivity.java
@@ -1,27 +1,39 @@
 package com.example.lastone;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyListActivity extends AppCompatActivity {
+public class MyListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     List<String> data=new ArrayList<String>();
+    ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_list);
-        GridView listView=findViewById(R.id.mylist);
-        for(int i=0;i<=100;i++){
-             data.add("item"+i);
+        ListView listView=findViewById(R.id.mylist);
+        for(int i=0;i<=10;i++) {
+            data.add("item" + i);
         }
-        ListAdapter adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+        adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
         listView.setAdapter(adapter);
+        listView.setEmptyView(findViewById(R.id.nodata));
+        listView.setOnItemClickListener(this);
 
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> listv, View view, int position, long id) {
+        Log.i("删除",""+position);
+        adapter.remove(listv.getItemAtPosition(position));
+        adapter.notifyDataSetChanged();
     }
 }
